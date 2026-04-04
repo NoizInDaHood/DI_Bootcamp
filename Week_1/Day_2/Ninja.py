0abe7b324c53e8216b5f8db97b3a649b630bfadb
NoizInDaHood <zionburtman@gmail.com>
2026-04-04 18:13:28 +0300

    Last update_cosmetic_display

--- Week_1/Day_2/Ninja.py
+++ Week_1/Day_2/Ninja.py
@@ -1,27 +1,31 @@
 
 
 highest_record = 0
-
-print("--- The 'No-A' Challenge ---")
-print("Type the longest sentence you can without using the letter 'A'!")
-
+#introduction to the game and instructions for the user.
+print("\n--- The 'No-A' Challenge ---")
+print("\n\nType the longest sentence you can without using the letter 'A'!")
+# create the infinite loop to keep game running until user types "exit"
 while True:
+    #I use the lower method to make sure that the game is not case sensistive when typing the letter a or exit
     sentence = input("\nEnter your sentence: ")
 
-    if "exit" in sentence.lower():
+    if sentence.lower() == "exit":
         print("Thanks for playing! Goodbye!")
         break
     if "a" in sentence.lower():
-        print(f"Game Over! Your sentence contained the letter 'A'.")
-        print(f"Final High Score: {highest_record}")
-        continue
+        print(f"\n\nGame Over! Your sentence contained the letter 'A'.")
+        print(f"\nFinal High Score: {highest_record}")
+        print("\n\nTry again to beat the highest score!\nOr type 'exit' to quit.")
+        continue #i first used break to end but then I realized that I wanted to give the user the option to play again, so I replaced it with continue to restart the loop and let the user try again.
 
-    current_length = len(sentence.strip())
+    current_length = len(sentence.strip()) #I use the strip method to remove empty spaces at the beginnign and the end of sentences
     
     if current_length > highest_record:
-        highest_record = current_length
-        print(f"New Record! {highest_record} characters!")
+        highest_record = current_length #I update the highest score if current lenght is greater than the highest score
+        print(f"\n\nNew Record! {highest_record} characters!")
+        print("\n\nTry again to beat the highest score!\nOr type 'exit' to quit.")
     else:
-        print(f"Solid effort ({current_length} chars), but you didn't beat {highest_record}.")
+        print(f"\n\nSolid effort ({current_length} chars), but you didn't beat {highest_record}.")
+        print("\n\nTry again to beat the highest score!\nOr type 'exit' to quit.")
 
         
