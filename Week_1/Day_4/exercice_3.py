c63bacf3cd4bf69da637085b43a4924ecce987e8
NoizInDaHood <zionburtman@gmail.com>
2026-04-07 15:55:54 +0300

    exercice_9

--- Week_1/Day_4/exercice_3.py
+++ Week_1/Day_4/exercice_3.py
@@ -1,37 +1,68 @@
-""" Exercise 8: Pizza Toppings
-
+""" Exercise 9: Cinemax Tickets
 Key Python Topics:
 
-Loops
+Conditionals
 Lists
-String formatting
+Loops
 
 
 Instructions:
 
-Write a loop that asks the user to enter pizza toppings one by one.
-Stop the loop when the user types 'quit'.
-For each topping entered, print:
-"Adding [topping] to your pizza."
-After exiting the loop, print all the toppings and the total cost of the pizza.
-The base price is $10, and each topping adds $2.50. """
-
-print("\nEnter pizza toppings one by one.\n\nPIZZA COST $10\n\nEach topping cost $2,50\n")
-print("\n\nType 'quit' when you are done\n")
-
-total_topping = []
-base_cost = 10
+Ask for the age of each person in a family who wants to buy a movie ticket.
+Calculate the total cost based on the following rules:
+Free for people under 3.
+$10 for people aged 3 to 12.
+$15 for anyone over 12.
+Print the total ticket cost.
 
 
-while True:
-    new_topping = input("Add your topping or type 'exit' to finish: ")
-    if new_topping.lower() == "exit":
-        total_cost = base_cost + (len(total_topping) * 2.5)
-        print(f"\nThanks for your order!\n\nThe total price is ${total_cost}. ")
-        print(f"Details:\nPizza base: $10\n +{total_topping}: ${len(total_topping) * 2.5:.2f}")
-        break
-    else:
-        total_topping.append(new_topping)
-        print(f"\n\nYou added {new_topping}")
+Bonus:
+
+Imagine a group of teenagers wants to see a restricted movie (only for ages 16–21).
+Write a program to:
+Ask for each person’s age.
+Remove anyone who isn’t allowed to watch.
+Print the final list of attendees. """
+
+how_much = []
+#how_many = int(input("\n\nHow many ticket would you like?: ")) # I ask the user how many tickets they want to buy and I convert the input to an integer. #
+# I use a for loop to iterate through the number of tickets and ask for the age of each person. I then use if-elif statements to determine the cost of each ticket based on the age and append the cost to the how_much list. Finally, I print the total cost by summing the how_much list. #
+#for i in range(how_many):
+#    input_age = int(input(f"\nType the age of the person {i+1}: "))# I ask for the age of each person and convert the input to an integer. #
+#    if input_age < 3:
+#        print("The ticket is free for this person.")
+#        how_much.append(0) # I use the fonction append to add the cost of the ticket to the how_much list. #
+#    elif input_age <13:
+#        print("$10 for people aged 3 to 12.")
+#        how_much.append(10)
+#    elif input_age > 12:
+#        print("$15 for people over the age 12.")
+#        how_much.append(15)   
+# I use the sum function to calculate the total cost of the tickets by summing the values in the how_much list. I then print the total cost. #
+#print(f"\n\nThe total cost of the tickets is: ${sum(how_much)}")
+
+""" Bonus: Imagine a group of teenagers wants to see a restricted movie (only for ages 16–21).""" 
+
+identity = {}
+number_of_people = int(input("\n\nHow many people are in the group? ")) # I ask the user how many people are in the group and convert the input to an integer. #
+
+for i in range(number_of_people):
+    name = input("\nPlease type your full name: ")
+    age = int(input("\nWhat is your age?: "))
+    identity[name] = age
+
+authorized_people = {name: age for name, age in identity.items() if 16 <= age <=21}
+
+print(f"Here are the people authorized to watch the movie: {list(authorized_people.keys())}")
+
+
+
+
+
+
+
+
+
+
 
 
--- Week_1/Day_4/exercices.py
+++ Week_1/Day_4/exercices.py
@@ -210,3 +210,73 @@ while True:
     else:
         total_topping.append(new_topping)
         print(f"\n\nYou added {new_topping}")
+
+
+""" Exercise 9: Cinemax Tickets
+Key Python Topics:
+
+Conditionals
+Lists
+Loops
+
+
+Instructions:
+
+Ask for the age of each person in a family who wants to buy a movie ticket.
+Calculate the total cost based on the following rules:
+Free for people under 3.
+$10 for people aged 3 to 12.
+$15 for anyone over 12.
+Print the total ticket cost.
+
+
+Bonus:
+
+Imagine a group of teenagers wants to see a restricted movie (only for ages 16–21).
+Write a program to:
+Ask for each person’s age.
+Remove anyone who isn’t allowed to watch.
+Print the final list of attendees. """
+
+how_much = []
+how_many = int(input("\n\nHow many ticket would you like?: ")) # I ask the user how many tickets they want to buy and I convert the input to an integer. #
+# I use a for loop to iterate through the number of tickets and ask for the age of each person. I then use if-elif statements to determine the cost of each ticket based on the age and append the cost to the how_much list. Finally, I print the total cost by summing the how_much list. #
+for i in range(how_many):
+    input_age = int(input(f"\nType the age of the person {i+1}: "))# I ask for the age of each person and convert the input to an integer. #
+    if input_age < 3:
+        print("The ticket is free for this person.")
+        how_much.append(0) # I use the fonction append to add the cost of the ticket to the how_much list. #
+    elif input_age <13:
+        print("$10 for people aged 3 to 12.")
+        how_much.append(10)
+    elif input_age > 12:
+        print("$15 for people over the age 12.")
+        how_much.append(15)   
+# I use the sum function to calculate the total cost of the tickets by summing the values in the how_much list. I then print the total cost. #
+print(f"\n\nThe total cost of the tickets is: ${sum(how_much)}")
+
+""" Bonus: Imagine a group of teenagers wants to see a restricted movie (only for ages 16–21).""" 
+
+identity = {}
+number_of_people = int(input("\n\nHow many people are in the group? ")) # I ask the user how many people are in the group and convert the input to an integer. #
+
+for i in range(number_of_people): # I use a for loop to iterate through the number of people in the group and ask for their name and age. I then store the name and age in a dictionary called identity. #
+    name = input("\nPlease type your full name: ")
+    age = int(input("\nWhat is your age?: "))
+    identity[name] = age # I use the name as the key and the age as the value in the identity dictionary. #
+
+authorized_people = {name: age for name, age in identity.items() if 16 <= age <=21} # I use a dictionary comprehension to create a new dictionary called authorized_people that contains only the people who are between the ages of 16 and 21. I check if the age is between 16 and 21 using a conditional statement in the comprehension. #
+
+print(f"Here are the people authorized to watch the movie: {list(authorized_people.keys())}") # I print the names of the authorized people by using the keys() method on the authorized_people dictionary and converting it to a list. #
+
+
+
+
+
+
+
+
+
+
+
+
