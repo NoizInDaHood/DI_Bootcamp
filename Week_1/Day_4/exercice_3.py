5d222180cee61efb91a6fa0f32d7ba670c66fba6
NoizInDaHood <zionburtman@gmail.com>
2026-04-14 14:14:34 +0300

    week_2_day2_exercices

--- Week_1/Day_4/exercice_3.py
+++ Week_1/Day_4/exercice_3.py
@@ -1,34 +1,3 @@
-""" Key Python Topics:
-input() function
-Strings and string manipulation
-Loops (for or while)
-Conditional statements (if)
 
 
-Instructions:
-1. Ask the user for a string.
-2. Write a program that processes the string to remove consecutive duplicate letters.
 
-The new string should only contain unique consecutive letters.
-For example, “ppoeemm” should become “poem” (removes consecutive duplicates like ‘pp’, ‘ee’, and ‘mm’).
-3. The program should print the modified string. """
-
-user_string = input("\nPlease type any word or sentence: ")
-
-
-def remove_duplicate(): 
-    internal_result = []
-    for i in range(len(user_string) - 1):
-        if user_string[i] == user_string[i+1]:
-            continue
-        else:
-            internal_result.append(user_string[i])
-    internal_result.append(user_string[-1])
-    string_result = "".join(internal_result)
-    return string_result
-
-cleaned_string = remove_duplicate()
-print(cleaned_string)
-user_string = input("\nPlease type any word or sentence: ")
-cleaned_string = remove_duplicate()
-print(cleaned_string)
