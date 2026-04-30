3762c35458bc3343dd18eaabd8a6d246873575a0
NoizInDaHood <zionburtman@gmail.com>
2026-04-30 16:11:52 +0300

    To_do_list

--- Week_3/Day_5/to_do_list/to_do_list.js
+++ Week_3/Day_5/to_do_list/to_do_list.js
@@ -1,19 +1,42 @@
-
 const form = document.getElementById("form")
 const input = document.getElementById("input")
 const list = document.getElementById("List")
 const btnAdd = document.getElementById("btnAdd")
-const btnDel = document.getElementById("btnDel")
+const btnClear = document.getElementById("btnClear")
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+
+form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
     const task = document.createElement("li");
     task.innerHTML = `
     <button class="delRow">X</button>
     <input type="checkbox" class="checkbox">
     <span class="task-ongoing">${input.value}</span>
     `
+    const btnDel = task.querySelector(".delRow");
+    const checkBox = task.querySelector(".checkbox");
+    const span = task.querySelector(".task-ongoing");
     task.classList.add("task");
     list.appendChild(task);
-    input.value = ""
+    input.value = "";
+    checkBox.addEventListener("change", (ev) => {
+    ev.preventDefault();
+    if (checkBox.checked) {
+        span.classList.remove("task-ongoing")
+        span.classList.add("task-done")
+    }
+    else {
+        span.classList.remove("task-done")
+        span.classList.add("task-ongoing")
+    }
+    });
+    btnDel.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        task.remove();
+    })
+});
+
+btnClear.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    list.innerHTML = ""
 });
