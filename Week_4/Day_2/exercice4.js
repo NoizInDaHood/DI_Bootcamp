3ab49069df3bd9aa9f5cb1ac780f4ae90f60e684
NoizInDaHood <zionburtman@gmail.com>
2026-05-07 12:02:29 +0300

    exercice4

---
+++ Week_4/Day_2/exercice4.js
@@ -0,0 +1,6 @@
+// Exercise 4 : Find the sum
+// Instructions
+// Create a one line function (ie. an arrow function) that receives two numbers as parameters and returns the sum.
+
+const makeSum = (a,b) => a+b;
+console.log(makeSum(55,75));
