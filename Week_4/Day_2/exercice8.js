5bf6ae0a86cd6e665346a52a992866c9a3c68ba5
NoizInDaHood <zionburtman@gmail.com>
2026-05-07 18:15:51 +0300

    exercice8

---
+++ Week_4/Day_2/exercice8.js
@@ -0,0 +1,31 @@
+// Exercise 8 : Juice Bar
+// Instructions
+// You will use nested functions, to open a new juice bar.
+
+// Part I:
+// The outer function named makeJuice receives 1 argument: the size of the beverage the client wants - small, medium or large.
+
+// The inner function named addIngredients receives 3 ingredients, and displays on the DOM a sentence like The client wants a <size drink> juice, containing <first ingredient>, <second ingredient>, <third ingredient>".
+
+// Invoke the inner function ONCE inside the outer function. Then invoke the outer function in the global scope.
+
+
+// Part II:
+// In the makeJuice function, create an empty array named ingredients.
+
+// The addIngredients function should now receive 3 ingredients, and push them into the ingredients array.
+
+// Create a new inner function named displayJuice that displays on the DOM a sentence like The client wants a <size drink> juice, containing <first ingredient>, <second ingredient>, <third ingredient>".
+
+// The client wants 6 ingredients in his juice, therefore, invoke the addIngredients function TWICE. Then invoke once the displayJuice function. Finally, invoke the makeJuice function in the global scope.
+
+function makeJuice(size) {
+    const ingredients = [];
+    (function addIngredients(ingr1, ingr2, ingr3) {
+        toDisplay = `The client wants a ${size} juice, containing ${ingr1}, ${ingr2} and ${ingr3}}`;
+        let drink = document.getElementById("drink");
+        drink.textContent = toDisplay;
+    })("rhum","orange juice","black cherry");
+
+};
+makeJuice("large");
